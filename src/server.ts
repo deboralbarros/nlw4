ff0c545194366e2c9a6be7d724e035efa6d541c3
Débora Barros <debora_lbarros@outlook.com>
2021-02-25 22:24:56 -0400

    feat: database, controllers, models and routes of application

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import {router} from "./routes"
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello" })
-})
-
-app.post("/", (req, res) => {
-    return res.status(200).json({ message: "Os dados foram salvos com sucesso!" })
-})
-
-
+app.use(express.json());
+app.use(router);
 app.listen(3333, () => console.log("Server is running on port 3333!"));
