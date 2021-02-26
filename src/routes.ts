d7787a63b09b8048e72639c19f637c7a27a9c180
Débora Barros <debora_lbarros@outlook.com>
2021-02-26 17:21:32 -0400

    feat: repositories, surveys, tests

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
-import { Router } from "express";
-import { UserController } from "./controllers/UserController";
+import {Router} from "express";
+import {UserController} from "./controllers/UserController";
+import {SurveysController} from "./controllers/SurveysController";
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveysController();
 
-router.post('/users', userController.create)
+router.post('/users', userController.create);
 
-export { router };
+router.get('/surveys', surveyController.show);
+router.post('/surveys', surveyController.create);
+
+export {router};
